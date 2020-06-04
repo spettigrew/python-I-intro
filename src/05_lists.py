068b60ad68e2738f9a5e61af1e242009971852f5
Sara Pettigrew <sarajp.ak@hotmail.com>
2020-06-04 15:15:25 -0600

    Lists in PYI completed

--- src/05_lists.py
+++ src/05_lists.py
@@ -23,11 +23,13 @@ print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
+x.insert(5, 99)
 print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
 len(x)
+print(x)
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
