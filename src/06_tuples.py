b2cbe17a8408e30b3cf8ee58363f269891f2e098
Sara Pettigrew <sarajp.ak@hotmail.com>
2020-06-05 18:23:04 -0600

    Defined function for tuple

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -33,11 +33,12 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 
 # Write a function `print_tuple` that prints all the values in a tuple
-
+def print_tuple (tup):
+    for item in tup:
+        print(item)  # Prints 1 2 5 7 99, one per line
 # YOUR CODE HERE
 t = (1, 2, 5, 7, 99)
-print(t)  # Prints 1 2 5 7 99, one per line
-
+print_tuple(t)
 # Declare a tuple of 1 element then print it
 u = (1,)  # What needs to be added to make this work?
 print(u)
